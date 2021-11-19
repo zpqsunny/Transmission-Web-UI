a7f8a3a8f789d8b10be0368350a9715a77d4d18d
zpq <zpqsunny@live.com>
2021-11-19 20:40:50 +0800

    Mobile.vue

--- src/views/Mobile.vue
+++ src/views/Mobile.vue
@@ -122,8 +122,10 @@
                       </div>
                     </v-list-item-subtitle>
                     <v-list-item-subtitle>
-                      <span>{{ item.uploadedEver | unitFormat }}</span> /
-                      <span>{{ item.totalSize | unitFormat }}</span>&nbsp;
+                      <template v-if="item.metadataPercentComplete >= 1">
+                        <span>{{ item.uploadedEver | unitFormat }}</span> /
+                        <span>{{ item.totalSize | unitFormat }}</span>&nbsp;
+                      </template>
                       <span v-if="item.eta > 0" class="text-no-wrap down-color">剩余 {{ item.eta | timeInterval }}</span>
                     </v-list-item-subtitle>
                   </v-list-item-content>
