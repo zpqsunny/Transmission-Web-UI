19463e8bcab044695536c306553791259511a6ff
ZpqSunny <zpqsunny@gmail.com>
2022-01-17 08:33:22 +0800

    fix

--- src/views/Mobile.vue
+++ src/views/Mobile.vue
@@ -125,7 +125,7 @@
                       </div>
                     </v-list-item-subtitle>
                     <v-list-item-subtitle>
-                      <template v-if="item.metadataPercentComplete >= 1">
+                      <template v-if="item.metadataPercentComplete === 1">
                         <span>{{ item.downloadedEver | unitFormat }} / {{ item.totalSize | unitFormat }}</span>
                       </template>
                       <span v-if="item.eta > 0" class="text-no-wrap down-color"> 剩余 {{ item.eta | timeInterval }}</span>
