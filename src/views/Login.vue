8f70d9793ed2dff17a5c95892fb0f38b47b5266b
zpq <zpqsunny@live.com>
2022-03-18 20:36:12 +0800

    Confirm.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -16,21 +16,32 @@
           </v-form>
         </v-container>
       </v-main>
+      <Confirm :v.sync="versionWarning.show" :title="versionWarning.title" :text="versionWarning.text" @cancel="cancel" @ok="go"></Confirm>
     </v-app>
   </div>
 </template>
 
 <script>
+import Confirm from '@/views/components/Confirm'
+
 export default {
   name: 'Login',
+  components: {
+    Confirm
+  },
   data: () => {
     return {
       api: {
-        url: 'http://127.0.0.1:9091/transmission/rpc',
+        url: 'http://localhost:8080/transmission/rpc',
         auth: false,
         username: '',
         password: ''
-      }
+      },
+      versionWarning: {
+        show: false,
+        title: '',
+        text: ''
+      },
     }
   },
   methods: {
@@ -42,7 +53,7 @@ export default {
       this.action()
     },
     action() {
-      this.$axios.post('',{
+      this.$axios.post('', {
         method: 'session-get',
         arguments: {
           fields: ['rpc-version', 'version']
@@ -51,24 +62,35 @@ export default {
         if (r.data.result === 'success') {
           let rpcVersion = this.$store.state.application.rpcVersion
           if (r.data.arguments['rpc-version'] < rpcVersion) {
-            this.$store.commit('showMessage',{type: 'error', title: 'Transmission RPC version must >= ' + rpcVersion})
+            this.versionWarning.title = '警告'
+            this.versionWarning.text = '当前RPC版本为' + r.data.arguments['rpc-version'] + ',小于建议版本' + rpcVersion + '是否继续?'
+            this.versionWarning.show = true
             return
           }
-          let re = /(Mobile|Android|iPad|Windows Phone|iPhone)/i
-          if (re.test(navigator.appVersion)) {
-            this.$router.push({path: '/m'})
-          } else {
-            this.$router.push({path: '/'})
-          }
+          this.go()
         }
       }).catch(error => {
-        if(error.response.status === 409) {
+        if (error.response.status === 409) {
           // 缺少头部 重试
           this.action()
           return
         }
-        this.$store.commit('showMessage',{type: 'error', title: 'Connect Fail'})
+        this.$store.commit('showMessage', {type: 'error', title: 'Connect Fail'})
       })
+    },
+    go() {
+      let re = /(Mobile|Android|iPad|Windows Phone|iPhone)/i
+      if (re.test(navigator.appVersion)) {
+        this.$router.push({path: '/m'})
+      } else {
+        this.$router.push({path: '/'})
+      }
+    },
+    cancel() {
+      localStorage.removeItem('url')
+      localStorage.removeItem('auth')
+      localStorage.removeItem('username')
+      localStorage.removeItem('password')
     }
   }
 }
