48b9fda66a882bc45be38e451a844d66a6360bc1
zpq <zpqsunny@live.com>
2022-10-05 12:29:40 +0800

    feat: tab i18n

--- chrome/background.js
+++ chrome/background.js
@@ -5,43 +5,47 @@ chrome.runtime.onInstalled.addListener(() => {
     title: chrome.i18n.getMessage('context_menus_title'),
     contexts: ['selection', 'link'],
   }, () => {
-    console.log('contextMenus created')
+    console.log('contextMenus created');
   })
-  console.log('onInstalled')
+  console.log('onInstalled');
 })
+let tId = -1
 // contextMenus onClicked
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId !== 'transmissionFastDownload') {
-    return
+    return;
+  }
+  if (tId < 0) {
+    console.log('not open tab')
+    return;
   }
   let url = info.linkUrl || info.selectionText
   chrome.runtime.sendMessage({
     extensionId: chrome.runtime.id,
     message: {url: url},
-  })
-  console.log('sendMessage ok')
+  });
+  console.log('sendMessage ok');
 })
-let tId = -1
 // action onClicked
 chrome.action.onClicked.addListener((tab) => {
   if (tId < 0) {
     chrome.tabs.create({url: chrome.runtime.getURL('index.html')})
       .then(t => {
-        console.log('tab created')
-        tId = t.id
+        console.log('tab created');
+        tId = t.id;
       })
-    return
+    return;
   }
   chrome.tabs.get(tId).then(t => {
     if (!t.active) {
-      chrome.tabs.update(tId, {active: true})
+      chrome.tabs.update(tId, {active: true});
     }
   })
 })
 // tabs onRemoved
 chrome.tabs.onRemoved.addListener((tabId, detachInfo) => {
   if (tabId === tId) {
-    console.log('tab removed')
-    tId = -1
+    console.log('tab removed');
+    tId = -1;
   }
 })
